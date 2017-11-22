96a49d96a2203c888e20e46f62ab017b5abe5e3f
Warrax <max.goudjo@hotmail.fr>
2017-11-22 17:32:16 +0100

    Menu navigation placé coté gauche

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,21 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+$(document).ready(function () {
+    $('#sidebarCollapseInfo').on('click', function () {
+        $('#sidebarInfo').toggleClass('active');
+    });
+});
+
+$(document).ready(function () {
+    $('#sidebarCollapseTrain').on('click', function () {
+        $('#sidebarTrain').toggleClass('active');
+    });
+});
+
+$(document).ready(function () {
+    $('#sidebarCollapseChall').on('click', function () {
+        $('#sidebarChall').toggleClass('active');
+    });
+});
