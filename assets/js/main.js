c0e4905635e7f264ad0496d487baf657fa62e6b0
Warrax <max.goudjo@hotmail.fr>
2017-11-22 10:23:14 +0100

    Barre de navigation sur le coté droit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+var $ = require('jquery');
+require('bootstrap-sass');
+
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
