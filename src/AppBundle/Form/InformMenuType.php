2bddf385758e0b9b5e0aedc41a8fd756a5b0433b
Pierre Lambert <pierrelambert41@gmail.com>
2017-11-30 16:55:11 +0100

    ajout de l'upload sur la partie ressource de l'admin

--- src/AppBundle/Form/InformMenuType.php
+++ src/AppBundle/Form/InformMenuType.php
@@ -3,10 +3,12 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class InformMenuType extends AbstractType
 {
@@ -23,9 +25,21 @@ class InformMenuType extends AbstractType
             ->add('link', UrlType ::class, array(
                 'label' => "Url du lien",
                 'label_attr' => array('class' => 'labels_admin')
+            ))
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => false,
+                'download_uri' => false,
+                'image_uri' => false,
+            ])
+            ->add('isMenu', CheckboxType::class, array(
+                'label' => "A cocher pour que le lien apparaisse dans le menu",
+                'required' => false,
+                'label_attr' => array('class' => 'labels_admin')
             ));
+
     }
-    
+
     /**
      * {@inheritdoc}
      */
