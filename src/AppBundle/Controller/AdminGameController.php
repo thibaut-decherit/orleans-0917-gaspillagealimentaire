64f2f6b2cec59e1bb0d6b6a5acedf6094d4a9f57
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-12-09 17:10:17 +0100

    refonte classe TrainMenu > Game

---
+++ src/AppBundle/Controller/AdminGameController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Game;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * AdminGameController controller.
+ *
+ * @Route("admin/jeux")
+ */
+class AdminGameController extends Controller
+{
+    /**
+     * Lists all game entities.
+     *
+     * @Route("/", name="admin_game_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $games = $em->getRepository('AppBundle:Game')->findAll();
+
+        return $this->render('admin/game/index.html.twig', array(
+            'games' => $games,
+        ));
+    }
+
+    /**
+     * Creates a new game entity.
+     *
+     * @Route("/new", name="game_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $game = new Game();
+        $form = $this->createForm('AppBundle\Form\GameNewType', $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($game);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_game_index', array('id' => $game->getId()));
+        }
+
+        return $this->render('admin/game/new.html.twig', array(
+            'game' => $game,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing game entity.
+     *
+     * @Route("/{id}/edit", name="game_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Game $game)
+    {
+        $deleteForm = $this->createDeleteForm($game);
+        $editForm = $this->createForm('AppBundle\Form\GameEditType', $game);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('game_edit', array('id' => $game->getId()));
+        }
+
+        return $this->render('admin/game/edit.html.twig', array(
+            'game' => $game,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a game entity.
+     *
+     * @Route("/{id}", name="game_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Game $game)
+    {
+        $form = $this->createDeleteForm($game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($game);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_game_index');
+    }
+
+    /**
+     * Creates a form to delete a game entity.
+     *
+     * @param game $game The game entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Game $game)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('game_delete', array('id' => $game->getId())))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+}
--- src/AppBundle/Entity/Game.php
+++ src/AppBundle/Entity/Game.php
@@ -4,17 +4,18 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 
 /**
- * TrainMenu
+ * Game
  *
- * @ORM\Table(name="train_menu")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\TrainMenuRepository")
+ * @ORM\Table(name="game")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GameRepository")
  * @Vich\Uploadable
  */
-class TrainMenu
+class Game
 {
     /**
      * @var int
@@ -28,18 +29,47 @@ class TrainMenu
     /**
      * @var string
      *
-     * @ORM\Column(name="title", type="string", length=255)
+     * @ORM\Column(name="title", type="string", length=100)
      *
      * @Assert\Length(
      * min = 3,
-     * minMessage = "Le nom de votre lien doit comporter au minimum {{ limit }} caractères.",
+     * minMessage = "Ce champ doit comporter au moins {{ limit }} caractères.",
      * )
-     *
      * @Assert\NotBlank(
      *    message = "Ce champ ne peut pas être vide.",
      * )
      */
     private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=100)
+     *
+     * @Assert\Length(
+     * min = 3,
+     * minMessage = "Ce champ doit comporter au moins {{ limit }} caractères.",
+     * )
+     * @Assert\NotBlank(
+     *    message = "Ce champ ne peut pas être vide.",
+     * )
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="summary", type="text")
+     *
+     * @Assert\Length(
+     * min = 3,
+     * minMessage = "Ce champ doit comporter au moins {{ limit }} caractères.",
+     * )
+     * @Assert\NotBlank(
+     *    message = "Ce champ ne peut pas être vide.",
+     * )
+     */
+    private $summary;
 
     /**
      * @var string
@@ -58,14 +88,14 @@ class TrainMenu
     /**
      * @var bool
      *
-     * @ORM\Column(name="is_menu", type="boolean")
+     * @ORM\Column(name="isMenu", type="boolean")
      */
     private $isMenu;
 
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      *
-     * @Vich\UploadableField(mapping="train_menu_image", fileNameProperty="imageName")
+     * @Vich\UploadableField(mapping="inform_menu_image", fileNameProperty="imageName")
      * @Assert\Image(
      *     maxSize="2M",
      *     maxSizeMessage="Ce fichier est trop grand, la limite est de 2 Mo.",
@@ -109,7 +139,7 @@ class TrainMenu
      *
      * @param string $title
      *
-     * @return TrainMenu
+     * @return Game
      */
     public function setTitle($title)
     {
@@ -129,11 +159,59 @@ class TrainMenu
     }
 
     /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Game
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     *
+     * @return Game
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
      * Set link
      *
      * @param string $link
      *
-     * @return TrainMenu
+     * @return Game
      */
     public function setLink($link)
     {
@@ -153,6 +231,30 @@ class TrainMenu
     }
 
     /**
+     * Set isMenu
+     *
+     * @param boolean $isMenu
+     *
+     * @return Game
+     */
+    public function setIsMenu($isMenu)
+    {
+        $this->isMenu = $isMenu;
+
+        return $this;
+    }
+
+    /**
+     * Get isMenu
+     *
+     * @return bool
+     */
+    public function getIsMenu()
+    {
+        return $this->isMenu;
+    }
+
+    /**
      * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
      * of 'UploadedFile' is injected into this setter to trigger the  update. If this
      * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
@@ -161,7 +263,7 @@ class TrainMenu
      *
      * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
      *
-     * @return TrainMenu
+     * @return InformMenu
      */
     public function setImageFile(File $image = null)
     {
@@ -189,7 +291,7 @@ class TrainMenu
      *
      * @param string $imageName
      *
-     * @return TrainMenu
+     * @return InformMenu
      */
     public function setImageName($imageName)
     {
@@ -213,7 +315,7 @@ class TrainMenu
      *
      * @param \DateTime $updatedAt
      *
-     * @return TrainMenu
+     * @return InformMenu
      */
     public function setUpdatedAt($updatedAt)
     {
@@ -231,28 +333,5 @@ class TrainMenu
     {
         return $this->updatedAt;
     }
-
-    /**
-     * Set isMenu
-     *
-     * @param boolean $isMenu
-     *
-     * @return TrainMenu
-     */
-    public function setIsMenu($isMenu)
-    {
-        $this->isMenu = $isMenu;
-
-        return $this;
-    }
-
-    /**
-     * Get isMenu
-     *
-     * @return boolean
-     */
-    public function getIsMenu()
-    {
-        return $this->isMenu;
-    }
 }
+
