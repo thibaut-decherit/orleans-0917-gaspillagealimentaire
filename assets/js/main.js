8dd3dbae1ce8ac40f706f51a947df5141c562ddb
Pierre Lambert <pierrelambert41@gmail.com>
2017-12-21 18:00:51 +0100

    cacher article JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 var $ = require('jquery');
 require('bootstrap-sass');
+
+$(document).ready(function () {
+
+    $(".pInfo").on('click', function (e) {
+        e.preventdefault();
+
+        $(".article[data-target=" + $(this).data('target') + "]").addClass('show');
+
+    });
+
+});
