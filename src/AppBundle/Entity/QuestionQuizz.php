e865c4c3fed01accb0ecbd5ea80beca4e0fab26a
Thibaut Decherit <thibaut.decherit@gmail.com>
2018-01-04 18:01:52 +0100

    corrections avant PR

--- src/AppBundle/Entity/QuestionQuizz.php
+++ src/AppBundle/Entity/QuestionQuizz.php
@@ -23,7 +23,7 @@ class QuestionQuizz
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\QuizzTitle", inversedBy="quizzQuestions")
      * @ORM\JoinColumn(nullable=false)
      */
-    private  $titleQuizz;
+    private $titleQuizz;
 
     /**
      * @var
@@ -105,6 +105,7 @@ class QuestionQuizz
     {
         return $this->quizzAnswers;
     }
+
     /**
      * Constructor
      */
